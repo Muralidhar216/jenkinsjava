a0569b1d7d6e66cffcc4ba472018361a437dc80d
Muralidhar <97168007+Muralidhar216@users.noreply.github.com>
2024-01-02 15:27:59 +0530

    Update App.java

--- src/main/java/com/mavenweb/jenkinsjava/App.java
+++ src/main/java/com/mavenweb/jenkinsjava/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! This is for Jenkins" );
+        System.out.println( "Hello World! This is for Jenkins updated" );
     }
 }
